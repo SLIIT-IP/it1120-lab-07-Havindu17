4cf6d118a4a18505228eeca826859fc3147f5275
Havindu17 <IT24103016@MY.SLIIT.LK>
2024-10-02 07:49:40 -0700

    IT24103016Lab7Q2A

---
+++ Q2/IT24103016Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24103016Lab7Q2A {
+public static void main (String[] args ) {
+
+for (int h = 1; h <= 5; h++) {
+for (int v = 1; v <= 5; v++) {
+
+System.out.print("$"); }
+System.out.println();
+           }
+        }
+    } 
+             
