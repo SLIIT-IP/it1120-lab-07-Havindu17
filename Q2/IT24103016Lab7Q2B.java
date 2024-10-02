7293d9c6599931b0192255513d86ddcf8b02d027
Havindu17 <IT24103016@MY.SLIIT.LK>
2024-10-02 07:50:03 -0700

    IT24103016Lab7Q2B

---
+++ Q2/IT24103016Lab7Q2B.java
@@ -0,0 +1,10 @@
+public class IT24103016Lab7Q2B {
+public static void main (String[] args ) {
+
+for (int h = 1; h <= 5; h++) {
+for (int v = 1; v <= h; v++) {
+System.out.print(  "*"); }
+System.out.println();
+           }
+       }
+  }
